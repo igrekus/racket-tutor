1e0e6262fd1f9f8595423145f267cfc359c0193b
igrekus <upload234@mail.ru>
2020-07-09 17:52:21 +0300

    WIP: add functional composition and extreme DRY versions

--- fizzbuzz.rkt
+++ fizzbuzz.rkt
@@ -31,3 +31,40 @@
           [(= 0 (modulo start 5)) "Buzz"]
           [else (~v start)])
         (string-join (map fb (range start end)) "\n"))))
+
+(define (fbcompose n)
+  (begin
+    (define fizz
+      (lambda (f)
+        (if (= 0 (modulo n 3))
+            (lambda (arg1)
+              (string-append "Fizz" (f "")))
+            f)))
+    (define buzz
+      (lambda (f)
+        (if (= 0 (modulo n 5))
+            (lambda (arg1)
+              (string-append "Buzz" (f "")))
+            f)))
+    (define id
+      (lambda (x)
+        x))
+    ((fizz (buzz id)) n)))
+
+(define (fbdry n)
+  (begin
+    (define test
+      (lambda (d s f)
+        (if (= 0 (modulo n d))
+            (lambda (arg1)
+              (string-append s (f "")))
+            f)))
+    (define (fizz n)
+      (test 3 "Fizz" n))
+    (define (buzz n)
+      (test 5 "Buzz" n))
+    (define id
+      (lambda (x)
+        x))
+    ((fizz (buzz id)) n)))
+
